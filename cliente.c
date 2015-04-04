132941067e70c3a9ac23f8018bf05f5a8581388f
fededri <fedetto94@hotmail.com>
2015-04-04 18:17:32 -0300

    cliente servidor con una sola conexion simultanea (funcional)

--- cliente.c
+++ cliente.c
@@ -12,7 +12,7 @@
 #include <signal.h>
 
 #define IP "127.0.0.1"
-#define PORT "9000"
+#define PORT "8500"
 #define MAXDATASIZE 1024
 
 int main()
@@ -21,38 +21,36 @@ int main()
 struct addrinfo hints, * serverInfo;
 int serverSocket;
 char message[MAXDATASIZE];
-int i;
+int resultadoOp;
 
-memset(&hints,0,sizeof hints);
-hints.ai_family = AF_INET;
+printf("Iniciando Cliente\n");
+memset(&hints,0,sizeof(hints));
+hints.ai_family = AF_UNSPEC;
 hints.ai_socktype = SOCK_STREAM;
 
-getaddrinfo(IP,PORT,&hints,&serverInfo);
 
+getaddrinfo(IP,PORT,&hints,&serverInfo);
 serverSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
 
-if (i = connect(serverSocket,serverInfo->ai_addr,serverInfo->ai_addrlen)==-1){
-	puts("Error al conectar");
-}
 
-
+resultadoOp = connect(serverSocket,serverInfo->ai_addr,serverInfo->ai_addrlen);
 freeaddrinfo(serverInfo);
 
-if ((i = recv(serverSocket,(void *) message, MAXDATASIZE, 0)!=0)){
-	printf ("%s",message);
-}
-else {
-	printf("El servidor no envio nada");
+if ( resultadoOp!=-1){
+	printf("Conectado al servidor. Ya puede enviar mensajes, escriba 'exit' para salir\n");
 }
 
-
-while(1){
+int enviar = 1;
+while(enviar){
 fgets(message,MAXDATASIZE,stdin);
-if(!strcmp(message,"salir")) send(serverSocket,(void *) message, MAXDATASIZE,0 );
-else {
-	break;
-}
+if(!strcmp(message,"exit\n")) enviar = 0;
+if (enviar) send(serverSocket, message, strlen(message)+1,0);
+
 }
 
-	return 0;
+close(serverSocket);
+return 0;
+
+
+
 }
--- server.c
+++ server.c
@@ -19,97 +19,60 @@
 #include <unistd.h>
 
 #define BACKLOG 10
-#define PORT "9000"
+#define PORT "8500"
 #define MAXDATASIZE 1024
 #define MAXCLIENTS 10
 
 int main(){
 struct addrinfo hints, *serverInfo;
-int max_descriptor = -1,resultado, socketClient;
-struct sockaddr_in addrClient;
-socklen_t addrClientSize;
-char message[MAXDATASIZE];
-fd_set read_sockets;
-
+int resultadoOp;
+int listenningSocket;
 
 memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;		// No importa si uso IPv4 o IPv6
 	hints.ai_flags = AI_PASSIVE;		// Asigna el address del localhost: 127.0.0.1
 	hints.ai_socktype = SOCK_STREAM;	
 
+printf("Iniciando Servidor...\n");
 
-resultado = getaddrinfo(NULL,PORT,&hints,&serverInfo);
-
-
-int listenningSocket;
-	listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
-
-printf("El socket que escucha es el %d\n",listenningSocket);
+getaddrinfo(NULL,PORT,&hints,&serverInfo);
+listenningSocket = socket(serverInfo->ai_family,serverInfo->ai_socktype,serverInfo->ai_protocol);
 
 
+printf("Socket escuchando numero %d\n",listenningSocket);
 
-bind(listenningSocket,serverInfo->ai_addr, serverInfo->ai_addrlen);
-	freeaddrinfo(serverInfo); // Ya no lo vamos a necesitar
+bind(listenningSocket,serverInfo->ai_addr,serverInfo->ai_addrlen);
+freeaddrinfo(serverInfo);
 
-puts("Escuchando...\n");
-listen(listenningSocket, BACKLOG);
-struct sockaddr_in addr;			// Esta estructura contendra los datos de la conexion del cliente. IP, puerto, etc.
-	socklen_t addrlen = sizeof(addr);
+printf("Socket bindeado\n");
 
-if( (socketClient = accept(listenningSocket, (struct sockaddr *) &addr, &addrlen)!=-1)) {
-	puts("Se conecto un cliente\n");
-		send(socketClient,"Bienvenido\n",MAXDATASIZE,0);
+printf("escuchando...\n");
+listen(listenningSocket,BACKLOG);
+
+struct sockaddr_in addr;
+socklen_t addrlen = sizeof(addr);
+
+int socketCliente = accept(listenningSocket, (struct sockaddr * ) &addr, &addrlen);
+char package[MAXDATASIZE];
+int status =1;
+
+if(socketCliente!=-1){
+	printf("Cliente conectado como socket %d",socketCliente);
+}
+else {
+	printf( "Error al conectar");
 }
 
-int status = 1;
-	while (status != 0){
-		status = recv(socketClient, (void*) message, MAXDATASIZE, 0);
-		if (status != 0) printf("%s", message);
-
-	} 
-
-
-
-/*
-while (1){
-FD_ZERO(&read_sockets);
-FD_SET(socketListener,&read_sockets);
-
-max_descriptor = socketListener;
-
-
-if((resultado = select(max_descriptor+1, &read_sockets,NULL,NULL,NULL))==-1){
-	puts("Error en select");
-}
-puts("Algo paso\n");
-if (FD_ISSET(socketListener,&read_sockets)){ //conexion entrante
-
-	if((socketClient = accept(socketListener, (struct sockaddr*) &addrClient, &addrClientSize))!=-1){
-		puts("Se conecto un cliente\n");
-		send(socketClient,"Bienvenido\n",MAXDATASIZE,0);
-		}
-	}
-
-	*/
-
-/*
-if (FD_ISSET(0,&read_sockets)){
-	if ( (resultado = recv(socketClient,&message,MAXDATASIZE,0)) != 0 ){
-	 puts(message);
-	}
-	else{
-		close(socketClient);
-		break;
-	}
+while(status!=0){
+status = recv(socketCliente, (void*) package, MAXDATASIZE,0);
+if(status!=0) printf("%s",package);
 }
 
-
-
-} // fin del ciclo
-*/
-
+puts("Cerrando Servidor\n");
+close(socketCliente);
 close(listenningSocket);
-puts("saliendo");
+
+
 	return 0;
 }
 
